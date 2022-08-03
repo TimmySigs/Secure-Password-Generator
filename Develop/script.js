e92135708d1ea46add5dabffa1286a386c3d03cb
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-08-03 16:42:47 -0500

    "added vars for prompt"

--- Develop/script.js
+++ Develop/script.js
@@ -4,17 +4,28 @@ var lower = "abcdefghijklmnopqrstuvwxyz"
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var number = "0123456789"
 var special = "!@#$%^&*()"
-var passwordLength = 12;
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+  var lower = confirm("Include lower case letters?");
+  var upper = confirm("Include upper case letters?");
+  var number = confirm("Include numbers?");
+  var special = confirm("Include special characters?");
+
+
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+function generatePassword(lower, upper, number, special) {
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
