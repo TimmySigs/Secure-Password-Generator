b8a35d9f71c0b8ba6a5d4fbe7b9b0de522944997
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-08-03 21:43:53 -0500

    "changed numbers to correct issue with pw length"

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var lower = "abcdefghijklmnopqrstuvwxyz"
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var number = "0123456789"
-var special = "!@#$%^&*~+-()"
+var special = "~`!@#$%^&*()-_=+"
 
 
 // Write password to the #password input
@@ -15,7 +15,7 @@ function writePassword() {
   var specialA = confirm("Include special characters?");
 
   while (lowerA === false && upperA === false && numberA === false && specialA === false) {
-    alert ("Must include at least one character field")
+    alert ("Must include at least one character field");
     var lowerA = confirm("Include lower case letters?");
     var upperA = confirm("Include upper case letters?");
     var numberA = confirm("Include numbers?");
@@ -25,38 +25,51 @@ function writePassword() {
 
   var passwordLength = (prompt("How many characters would you like to use (8-128)"));
 
-  while (passwordLength <=9 || >= 129) {
+  while (passwordLength <=7 || passwordLength >= 129) {
     alert("Password must be between 8 to 128 characters")
-    var passwordLength = (prompt("How many characters would you like to use (8-128)"));
+    var passwordLength = (prompt("How many characters would you like to use 8-128"));
   }
 
 var passwordString = "";
+var gtdchar = "";
 
 if (lowerA) {
   passwordString += lower;
+  gtdchar += lower.charAt(Math.floor(Math.random() * lower.length)) 
 }
 if (upperA) {
   passwordString += upper;
+  gtdchar += upper.charAt(Math.floor(Math.random() * upper.length ))
 }
 if (numberA) {
   passwordString += number;
+  gtdchar += number.charAt(Math.floor(Math.random() * number.length ))
 }
 if (specialA) {
   passwordString += special;
+  gtdchar += special.charAt(Math.floor(Math.random() * special.length ))
 }
+console.log(gtdchar);
 
 var newPassword = "";
 
-for (var i =1; <= passwordLength; i++) {
+for (var i = 1; i <= passwordLength; i++) {
   newPassword += passwordString.charAt(Math.floor(Math.random() * passwordString.length));
 }
 
+// var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
 passwordText.value = newPassword;
 
-// var password = generatePassword();
-// var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+console.log(newPassword)
+console.log(passwordString);
+
+
+
+
+  // passwordText.value = password;
 
 }
 
